965686576f96d1d8eb74a6ddd3f378a68af27bac
ltoddy <taoliu0509@gmail.com>
2020-09-18 16:52:20 +0800

    change shebang

--- scripts/install-kubernetes-images-for-mac.sh
+++ scripts/install-kubernetes-images-for-mac.sh
@@ -1,24 +1,24 @@
-#! /bin/bash
+#! /usr/bin/env bash
 
 images=(
-  k8s.gcr.io/kube-proxy:v1.16.5,gotok8s/kube-proxy:v1.16.5
-  k8s.gcr.io/kube-controller-manager:v1.16.5,gotok8s/kube-controller-manager:v1.16.5
-  k8s.gcr.io/kube-scheduler:v1.16.5,gotok8s/kube-scheduler:v1.16.5
-  k8s.gcr.io/kube-apiserver:v1.16.5,gotok8s/kube-apiserver:v1.16.5
-  k8s.gcr.io/coredns:1.6.2,gotok8s/coredns:1.6.2
-  k8s.gcr.io/pause:3.1,gotok8s/pause:3.1
-  k8s.gcr.io/etcd:3.3.15-0,gotok8s/etcd:3.3.15-0
+    k8s.gcr.io/kube-proxy:v1.16.5,gotok8s/kube-proxy:v1.16.5
+    k8s.gcr.io/kube-controller-manager:v1.16.5,gotok8s/kube-controller-manager:v1.16.5
+    k8s.gcr.io/kube-scheduler:v1.16.5,gotok8s/kube-scheduler:v1.16.5
+    k8s.gcr.io/kube-apiserver:v1.16.5,gotok8s/kube-apiserver:v1.16.5
+    k8s.gcr.io/coredns:1.6.2,gotok8s/coredns:1.6.2
+    k8s.gcr.io/pause:3.1,gotok8s/pause:3.1
+    k8s.gcr.io/etcd:3.3.15-0,gotok8s/etcd:3.3.15-0
 )
 
 echo "start install docker images"
 
 for image in ${images[@]}; do
-  image=(${image//,/ })
-  key=${image[0]}
-  value=${image[1]}
-  docker pull ${value}
-  docker tag ${value} ${key}
-  docker rmi ${value}
+    image=(${image//,/ })
+    key=${image[0]}
+    value=${image[1]}
+    docker pull ${value}
+    docker tag ${value} ${key}
+    docker rmi ${value}
 done
 
 echo "finished install docker images"
