12ca9b4a4b432ec59c7316e41233252eaa7c281f
ltoddy <taoliu0509@gmail.com>
2021-05-30 11:03:17 +0800

    feat: whoami

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+use std::env;
+
+use log::Level;
+use tide::{Request, Response};
+
+async fn whoami(_req: Request<()>) -> tide::Result<Response> {
+    Ok("ltoddy".into())
+}
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    env::set_var("RUST_LOG", Level::Info.as_str());
+    env_logger::init();
+
+    let mut app = tide::new();
+    app.with(driftwood::ApacheCommonLogger);
+
+    app.at("/whoami").get(whoami);
+
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
 }
